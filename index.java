f52afce31c292a684d3eafc2bff1b96a171aeb6d
ZainabKhan944 <zyk0517@gmail.com>
2020-10-19 23:34:08 +0000

    Finished most of it, but couldn't manage to get the ellipses to disappear under the rectange

--- index.java
+++ index.java
@@ -1,32 +1,23 @@
-  x = 100;
-   y = 100;
-
 void setup()  {
 	size(500, 500);
+  frameRate(20);
 }
+
 
 void draw() {
-// Step 1: Write code that draws the following screenshots 
-// with hard-coded values. (Feel free to use colors 
-// instead of grayscale.)
 
-// Step 2: Replace all of the hard-coded numbers with 
-// variables.
-
-// Step 3: Write assignment operations in draw() that 
-// change the value of the variables.
-// For example, “variable1 = variable1 + 2;”. 
-
-// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
-  background(225);
-  fill(180);
-  rect(20, 20, 125, 125);
-  fill(255);
-  ellipse(x, y, 75, 75);
-   x = x + 11;
-   y = y + 10;
-  
-
-
+  fill(int (random(0, 255)), int (random(1, 255)), int (random(1, 255)));
+  ellipse(mouseX +  int (random(-10, 30)),mouseY + int (random(-10, 30)), 10, 10);
 }
+
+void mousePressed() {
+ 
+ background(255);
+  fill(int (random(1, 225)), int (random(1, 225)), int (random(1, 225)));
+   rect(int (random(1, 500)), int (random (1,500)), int (random (75,200)), int (random (75,240)));
+ }
+
+
+
+
+
