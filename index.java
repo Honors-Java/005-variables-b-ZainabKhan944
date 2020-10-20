0a0cc5b43be646d7d6b5b6f18a5fa30abc8582f1
ZainabKhan944 <zyk0517@gmail.com>
2020-10-20 15:05:25 +0000

    I rewrote my whole code because I made a lot of errors on the previous one

--- index.java
+++ index.java
@@ -1,20 +1,34 @@
+int rectX = 100;
+int rectY = 100;
+int rectW = 200;
+int rectH= 200;
+int r = random(0,255);
+int g = random(0,255);
+int b = random(0,255);
+
 void setup()  {
 	size(500, 500);
-  frameRate(20);
+  frameRate(30);
 }
 
 
 void draw() {
-
-  fill(int (random(0, 255)), int (random(1, 255)), int (random(1, 255)));
-  ellipse(mouseX +  int (random(-10, 30)),mouseY + int (random(-10, 30)), 10, 10);
+  stroke(0);
+  fill(r, g, b);
+  rect(rectX, rectY, rectW, rectH);
+  fill((int) random(0,255), (int) random(0,255), (int) random(0,255));
+  ellipse(mouseX +  int (random(-30, 30)),mouseY + int (random(-30, 30)), 10, 10);
 }
 
 void mousePressed() {
- 
+ r = (int) random(0,255); 
+ g = (int) random(0,255);
+ b = (int) random(0,255);
  background(255);
-  fill(int (random(1, 225)), int (random(1, 225)), int (random(1, 225)));
-   rect(int (random(1, 500)), int (random (1,500)), int (random (75,200)), int (random (75,240)));
+ rectX = (int) random(0,500);
+ rectY = (int) random(0,500);
+ rectWidth = (int) random (50, 400);
+ rectHeight = (int) random (50,400);
  }
 
 
